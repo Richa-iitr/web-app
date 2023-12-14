bba5c562d087ef3051a0006d54aae4ed3019f9be
Richa-iitr <richa@cs.iitr.ac.in>
2023-12-14 23:41:32 +0530

    test:file create and write

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,9 @@
-#[macro_use] extern crate nickel;
-use nickel::Nickel;
+use std::fs::File;
+use std::io::prelude::*;
 
-fn print_hello() -> &'static str {
-    "hello world!"
-}
 
-fn main() {
-    let mut server = Nickel::new();
-    server.utilize(router! {
-        get "**" => |_req,_res| {
-            print_hello()
-        }
-    });
-    //listen on server at localhost:6767
-    server.listen("127.0.0.1:6767");
-
-    //to run: cargo run
-    //to test: curl http://localhost:6767
+fn main() -> std::io::Result<()>{
+    let mut file = File::create("foo.txt")?;
+    file.write_all(b"Hello world!")?;
+    Ok(())
 }
