736ca5cbaaf6111d521c25ea4f624f7770459101
Richa-iitr <richa@cs.iitr.ac.in>
2023-12-16 10:55:14 +0530

    log date file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::fs::File;
+use std::fs::{File,OpenOptions};
 use std::io::prelude::*;
 extern crate chrono;
 use chrono::*;
 
 fn log_something(filename: &'static str) -> std::io::Result<()> {
-    let mut f=(File::create(filename))?;
+    let mut f=OpenOptions::new().append(true).create(true).open(filename)?;
     let local:DateTime<Local> = Local::now();
     let time_str = local.format("%a, %b %d %Y %I:%M:%S %p\n").to_string();
     let str = time_str.as_bytes();
