5c39d4b70e20e44b68842d7a5a27a2fa330dd38e
Richa-iitr <richa@cs.iitr.ac.in>
2023-12-16 12:26:31 +0530

    logger web-app

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
-use std::fs::{File,OpenOptions};
-use std::io::prelude::*;
+#[macro_use] extern crate nickel;
+use nickel::Nickel;
+
 extern crate chrono;
 use chrono::*;
+use std::fs::{File, OpenOptions};
+use std::io;
+use std::io::prelude::*;
 
-fn log_something(filename: &'static str) -> std::io::Result<()> {
-    let mut f=OpenOptions::new().append(true).create(true).open(filename)?;
-    let local:DateTime<Local> = Local::now();
-    let time_str = local.format("%a, %b %d %Y %I:%M:%S %p\n").to_string();
-    let str = time_str.as_bytes();
-    (f.write_all(str));
+fn get_dateTime(filename:&'static str) -> io::Result<()> {
+    let mut f = OpenOptions::new().append(true).create(true).open(filename)?;
+    let local: DateTime<Local> = Local::now();
+    let local_str = local.format("%a, %b %d %Y %I:%M:%S %p\n").to_string();
+    let time = local_str.as_bytes();
+    f.write_all(time);
     Ok(())
 }
-fn main() {
-    match log_something("log.txt") {
-        Ok(..) => println!("Created and written"),
-        Err(..) => println!("ERROR!")
+fn log_dateTime(filename:&'static str)  {
+    match get_dateTime(filename) {
+        Ok(..) => println!("Created and logged"),
+        Err(..) => println!("Error encountered")
     }
+}
+
+fn main() {
+    let mut server = Nickel::new();
+    server.utilize(router! {
+        get "**" => |_req,_res| {
+            log_dateTime("log.txt")
+        }
+    });
+    //listen on server at localhost:6767
+    server.listen("127.0.0.1:6767");
+
+    //to run: cargo run
+    //to test: curl http://localhost:6767
 }
